d034eaf62aaf9dedc4222f39353e9993f238596b
kemsty <steeve.kemgne@orange.com>
2020-06-26 15:03:03 +0100

    finish development

--- index.js
+++ index.js
@@ -4,21 +4,48 @@ const clear = require("clear");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const inquirer = require("./lib/inquirer");
+const {
+  getGestionnaireForClient,
+  createCsvFile,
+  createCsvRow,
+} = require("./lib/helpers");
+
+const CLI = require("clui");
+const Spinner = CLI.Spinner;
 
 clear();
 
 console.log(
-  chalk`{yellow ${figlet.textSync("Projet RH", { horizontalLayout: "full" })}}`
+  chalk`{yellow ${figlet.textSync("AttributionClient RH", {
+    horizontalLayout: "full",
+  })}}`
 );
 
 console.log(
-  chalk`{green.dim.strikethrough ${"Projet permettant de repartir des projets"}}`
+  chalk`{green.dim.strikethrough ${"Projet permettant de repartir des clients à des gestionnaires"}}`
 );
 
 const run = async () => {
-  const filePath = await inquirer.askFilePath();
+  const status = new Spinner(
+    "Veuillez Patienter S'il vous plaît, Traitement en cours"
+  );
 
-  console.log(filePath);
+  try {
+    const filesPath = await inquirer.askFilePath();
+
+    status.start();
+
+    const listOfClients = await getGestionnaireForClient(filesPath, status);
+
+    createCsvFile(createCsvRow(listOfClients), status);
+  } catch (err) {
+    console.log(
+      chalk`{red ${"Erreur lors de la répartition des gestionnaires, Veuillez réessayer s'il vous plaît!"}}`
+    );
+    return;
+  } finally {
+    status.stop();
+  }
 };
 
 run();
